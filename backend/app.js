f85ed3384eccc509d74526a2c64a38b891e64aef
ktulu92 <sludgekicker@gmail.com>
2021-03-02 20:24:19 +0300

    change port

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-const PORT = 3004;
+const PORT = 3000;
 const app = express();
 app.use(require('cors')());
 
