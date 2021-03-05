4e8d845e4db13dc8ab197262377eec8ceb97dd65
ktulu92 <sludgekicker@gmail.com>
2021-03-05 20:35:22 +0300

    bugfix

--- backend/models/users.js
+++ backend/models/users.js
@@ -57,7 +57,7 @@ userSchema.statics.findUserByCredentials = function (email, password) {
       if (!user) {
         return Promise.reject(new Error('Wrong login'));
       }
-      return bcrypt.compare(password.user.password).then((matched) => {
+      return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
           return Promise.reject(new Error('Wrong password'));
         }
