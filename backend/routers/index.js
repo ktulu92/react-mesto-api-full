3e517a4516c87f6e0bf783f539bdf574827f61cb
ktulu92 <sludgekicker@gmail.com>
2021-03-05 12:24:30 +0300

    bugfix

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const cors = require('cors');
 const auth = require('../middlewares/auth');
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { login, createUser } = require('../controllers/users');
+
+const options = {
+  origin: [
+    '*',
+    // 'http://localhost:8080',
+    // 'http://ktulu92.students.nomoredomains.monster',
+    // 'https://ktulu92.students.nomoredomains.monster',
+    // 'https://api.ktulu92.students.nomoredomains.monster',
+    // 'http://api.ktulu92.students.nomoredomains.monster',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'autorization'],
+  credentials: true,
+};
+
+router.use('*', cors(options)); // ПЕРВЫМ!
 
 router.post(
   '/signup',
