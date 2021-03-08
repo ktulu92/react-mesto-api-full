50d79b53981318c12287a59dc0e521e6ec4efdb0
ktulu92 <sludgekicker@gmail.com>
2021-03-08 23:39:19 +0300

    32nf rev

--- backend/app.js
+++ backend/app.js
@@ -45,24 +45,22 @@ app.use('/', router);
 app.use(errorLogger); // подключаем логгер ошибок
 
 app.use((err, req, res, next) => {
-  let { statusCode = 500, message } = err;
-  if (err.name === 'ValidationError') {
-    statusCode = 400;
-    message = 'Ошибка валидации';
-  }
-  if (err.name === 'CastError') {
-    statusCode = 400;
-    message = 'Передан некорректный токен';
-  }
-  if (err.name === 'MongoError' && err.code === 11000) {
-    statusCode = 409;
-    message = 'Пользователь с таким email уже есть';
-  }
+  const { statusCode = 500, message } = err;
+  // if (err.name === 'ValidationError') {
+  //   statusCode = 400;
+  //   message = 'Ошибка валидации';
+  // }
+  // if (err.name === 'CastError') {
+  //   statusCode = 400;
+  //   message = 'Передан некорректный токен';
+  // }
+  // if (err.name === 'MongoError' && err.code === 11000) {
+  //   statusCode = 409;
+  //   message = 'Пользователь с таким email уже есть';
+  // }
   res.status(statusCode).send({
     message: statusCode === 500 ? 'Ошибка сервера' : message,
   });
-
-  next();
 });
 
 app.listen(PORT, () => {
