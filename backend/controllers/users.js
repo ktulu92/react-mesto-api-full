d456274c79f9d900b4037523a3e2c130fc934146
ktulu92 <sludgekicker@gmail.com>
2021-03-08 12:32:18 +0300

    2nf rev

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -40,58 +40,27 @@ const createUser = (req, res, next) => {
       avatar,
       email,
       password: hash,
+    }))
+
+    .then((user) => {
+      res.status(200).res.send({
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+        _id: user._id,
+      });
     })
-      .catch((err) => {
-        if (err.name === 'MongoError') {
-          throw new BadRequestError('Этот адрес уже используется');
-        }
-        throw new BadRequestError(`Некорректный запрос: ${err.message}`);
-      })
-      .then((user) => {
-        res.status(200).res.send({
-          name: user.name,
-          about: user.about,
-          avatar: user.avatar,
-          email: user.email,
-          _id: user._id,
-        });
-      }))
-    .catch(next);
+    .catch((err) => {
+      if (err.name === 'MongoError') {
+        next(new BadRequestError('Этот адрес уже используется'));
+      }
+      // else {
+      //   next(new ServerError('Ошибка на сервере'));
+      // }
+      next(err);
+    });
 };
-
-// const createUser = (req, res, next) => {
-//   const {
-//     name, about, avatar, email, password,
-//   } = req.body;
-
-//   bcrypt
-//     .hash(password, 10)
-//     .then((hash) => User.create({
-//       name,
-//       about,
-//       avatar,
-//       email,
-//       password: hash,
-//     }))
-
-//     .then((user) => {
-//       res.status(200).res.send({
-//         name: user.name,
-//         about: user.about,
-//         avatar: user.avatar,
-//         email: user.email,
-//         _id: user._id,
-//       });
-//     })
-//     .catch((err) => {
-//       if (err.name === 'MongoError') {
-//         throw new BadRequestError('Этот адрес уже используется');
-//       } else {
-//         next(new ServerError('Ошибка на сервере'));
-//       }
-//       next(err);
-//     });
-// };
 
 const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
