993e8d8a124dd1c831a88b5d88113c0a84908ea9
ktulu92 <sludgekicker@gmail.com>
2021-03-09 03:12:51 +0300

    32nf rev

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -100,12 +100,12 @@ function App() {
       .then((res) => { 
         
         // localStorage.setItem("jwt", res.token); 
-        
+        if(data){
           setIsRegisterd(true); 
           setIsInfoToolTipPopupOpen(true); 
           history.push("/signin"); 
 
-        
+        }
         
       }) 
       .catch((err) => { 
