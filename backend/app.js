90460365b2bcad32f5c339f1a10db3295317f837
ktulu92 <sludgekicker@gmail.com>
2021-03-09 05:37:15 +0300

    bugfix

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,8 @@ app.use(errorLogger); // подключаем логгер ошибок
 
 // app.use((err, req, res, next) => {
 //   const { statusCode = 500, message } = err;
-//   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+//   res.status(statusCode).send({ message:
+// statusCode === 500 ? 'На сервере произошла ошибка' : message });
 //   next();
 // });
 
